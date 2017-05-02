20d9b8df8f3a61a2f2659fe3b052544c2d349fb5
utkuufuk <utkuufuk@gmail.com>
2017-05-02 03:49:28 +0300

    added solution to HW1

---
+++ hw1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 40;
+
+template <class Summable>
+inline void sum(Summable& p, Summable n, Summable d[])
+{
+	p = 0;
+
+	for (Summable i = 0; i < n; ++i)
+	{
+		p = p + d[i];	
+	}
+}
+
+int main()
+{
+	int accum = 0;
+	int data[N];
+	
+	for (int i = 0; i < N; ++i)
+	{
+		data[i] = i;
+	}	
+	sum(accum, N, data);
+	cout << "sum is " << accum << endl;
+}
